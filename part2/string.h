5ab8fd952d5a700be1f14f4a1074cd584f08f0ae
steven-horn <31835354+steven-horn@users.noreply.github.com>
2020-01-27 15:09:02 -0500

    length, hash

--- part2/string.h
+++ part2/string.h
@@ -15,23 +15,29 @@ public:
   char* val_; // The value of the string as a character array.
   size_t size_; // The length of the string.
 
-  // Constructs empty string.
-  String();
-
   // Constructs a string from a char*.
+  // @param c: pointer to an array of char
   String(char* c);
 
   // Constructs a string when passed a const char*.
+  // @param c: pointer to an immutable array of char
   String(const char* c);
 
   // Destructs a string.
   ~String();
 
-  // Generates a hash value for a string.
-  size_t hash_me_();
+  // Get the length of the string.
+  // @return the length of the string
+  size_t length();
 
   // Compares two strings for equality.
+  // @param other: pointer to an object
+  // @return true if other equals this string
   bool equals(Object * other);
+
+  // Overriden hash function.
+  // @return hash value of this string
+  size_t hash();
 
   // Concatenates other onto the current string.
   String * concat(String * other);
