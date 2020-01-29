8cd09492ff8ad714bea96a55f8e0981320510b7e
steven-horn <31835354+steven-horn@users.noreply.github.com>
2020-01-29 09:11:37 -0500

    added objectqueue,, stringqueue

--- part2/queue.h
+++ part2/queue.h
@@ -5,19 +5,19 @@
 #include "object.h"
 
 /**
-  * A Dynammic First-In-First-Out data structure
+  * A Dynamic First-In-First-Out data structure
   * that can hold an Object or a String.
   * This queue does not have a maximum capacity.
   * authors: horn.s@husky.neu.edu, armani.a@husky.neu.edu
   */
-class Queue {
+class ObjectQueue {
 public:
 
   // Constructor for the queue.
-  Queue();
+  ObjectQueue();
 
   // Destructor for the queue.
-  ~Queue();
+  ~ObjectQueue();
 
   // Add element toAdd to queue.
   void push(Object* toAdd);
@@ -33,4 +33,35 @@ public:
 
   // Returns the element at the front of the queue without removal.
   Object* peek();
+};
+
+/**
+  * A Dynamic First-In-First-Out data structure
+  * that can hold a String.
+  * This queue does not have a maximum capacity.
+  * authors: horn.s@husky.neu.edu, armani.a@husky.neu.edu
+  */
+class StringQueue {
+public:
+
+  // Constructor for the queue.
+  StringQueue();
+
+  // Destructor for the queue.
+  ~StringQueue();
+
+  // Add element toAdd to queue.
+  void push(String* toAdd);
+
+  // Removes element from front of Queue, returning the element.
+  String* pop();
+
+  // Returns the size of this queue at the moment.
+  size_t size();
+
+  // Returns whether the queue has any elements.
+  bool isEmpty();
+
+  // Returns the element at the front of the queue without removal.
+  String* peek();
 };
